f5dd5b815e0e1cec3f08508c140a21e5848a1543
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-10-22 07:59:31 +0300

    remove phpdoc

--- src/Traits/TestsTraits/TestsModelHelperTrait.php
+++ src/Traits/TestsTraits/TestsModelHelperTrait.php
@@ -7,16 +7,12 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Laraneat\Modules\Exceptions\InvalidSubject;
-use Throwable;
 
 /**
  * @mixin \Illuminate\Foundation\Testing\TestCase
  */
 trait TestsModelHelperTrait
 {
-    /**
-     * @throws Throwable
-     */
     public function assertExistsModelWhereColumns($subject, array $columns): void
     {
         $this->assertTrue(
