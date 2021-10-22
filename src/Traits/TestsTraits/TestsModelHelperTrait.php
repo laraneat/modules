cefe7317e584e4e1a716d40e20cfc7c8083f2343
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-10-22 07:44:32 +0300

    update `TestsModelHelperTrait`: - rename `assertExistsModelWithAttributes` to `assertExistsModelWhereColumns` - add `getModelsWhereColumns`, `makeQueryWhereColumns` - update snapshots

--- src/Traits/TestsTraits/TestsModelHelperTrait.php
+++ src/Traits/TestsTraits/TestsModelHelperTrait.php
@@ -3,6 +3,7 @@
 namespace Laraneat\Modules\Traits\TestsTraits;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Laraneat\Modules\Exceptions\InvalidSubject;
@@ -16,7 +17,20 @@ trait TestsModelHelperTrait
     /**
      * @throws Throwable
      */
-    public function assertExistsModelWithAttributes($subject, array $attributes): void
+    public function assertExistsModelWhereColumns($subject, array $columns): void
+    {
+        $this->assertTrue(
+            $this->makeQueryWhereColumns($subject, $columns)->exists(),
+            sprintf('Model where columns (%s) not exists', http_build_query($columns))
+        );
+    }
+
+    public function getModelsWhereColumns($subject, array $columns): Collection
+    {
+        return $this->makeQueryWhereColumns($subject, $columns)->get();
+    }
+
+    public function makeQueryWhereColumns($subject, array $columns): Builder|Relation
     {
         if (is_subclass_of($subject, Model::class)) {
             $subject = $subject::query();
@@ -27,10 +41,10 @@ trait TestsModelHelperTrait
             InvalidSubject::make($subject)
         );
 
-        foreach ($attributes as $key => $value) {
-            $subject->where($key, $value);
+        foreach ($columns as $column => $value) {
+            $subject->where($column, $value);
         }
 
-        $this->assertTrue($subject->exists());
+        return $subject;
     }
 }
