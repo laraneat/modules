699eae527bceeadf49ec9b6fdab60d44af44c513
Jackardios <salahutdinov.salavat@gmail.com>
2024-01-22 14:10:32 +0300

    refactor

--- src/Providers/BootstrapServiceProvider.php
+++ src/Providers/BootstrapServiceProvider.php
@@ -8,11 +8,18 @@ use Laraneat\Modules\Contracts\RepositoryInterface;
 class BootstrapServiceProvider extends ServiceProvider
 {
     /**
+     * Register the provider.
+     */
+    public function register(): void
+    {
+        $this->app[RepositoryInterface::class]->register();
+    }
+
+    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        $this->app[RepositoryInterface::class]->register();
         $this->app[RepositoryInterface::class]->boot();
     }
 }
