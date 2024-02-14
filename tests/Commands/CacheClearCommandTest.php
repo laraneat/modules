f1c6e269d3ae0e0839eefce22eb1f0730b7216b1
Jackardios <salahutdinov.salavat@gmail.com>
2024-02-14 20:34:35 +0300

    update tests

--- tests/Commands/CacheClearCommandTest.php
+++ tests/Commands/CacheClearCommandTest.php
@@ -3,17 +3,6 @@
 use Laraneat\Modules\ModulesRepository;
 use Mockery\MockInterface;
 
-beforeEach(function() {
-    $this->setVendorModules([
-        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/vendor/laraneat/foo'),
-        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/vendor/laraneat/bar'),
-    ]);
-    $this->setAppModules([
-        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/app/Article'),
-        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/app/Author'),
-    ]);
-});
-
 it('clears vendor modules cache', function () {
     $this->mock(ModulesRepository::class, function(MockInterface $mock) {
         $mock->shouldReceive('pruneVendorModulesManifest')->once();
--- tests/Commands/ListCommandTest.php
+++ tests/Commands/ListCommandTest.php
@@ -1,39 +1,41 @@
 <?php
 
 beforeEach(function() {
-    $this->setVendorModules([
-        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/vendor/laraneat/foo'),
-        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/vendor/laraneat/bar'),
-    ]);
     $this->setAppModules([
         realpath(__DIR__ . '/../fixtures/stubs/modules/valid/app/Article'),
         realpath(__DIR__ . '/../fixtures/stubs/modules/valid/app/Author'),
+    ], $this->app->basePath('/app/Modules'));
+    $this->setVendorModules([
+        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/vendor/laraneat/foo'),
+        realpath(__DIR__ . '/../fixtures/stubs/modules/valid/vendor/laraneat/bar'),
     ]);
 });
 
 it('outputs a table of app modules', function () {
     $this->artisan('module:list --app')
         ->expectsTable(['Package Name', 'Namespace', 'Path'], [
-            ['laraneat/article', 'App\\Modules\\Article', realpath(__DIR__ . '/../fixtures/stubs/modules/valid/app/Article')],
-            ['laraneat/author', 'App\\Modules\\Author', realpath(__DIR__ . '/../fixtures/stubs/modules/valid/app/Author')],
+            ['laraneat/article', 'App\\Modules\\Article', $this->app->basePath('/app/Modules/Article')],
+            ['laraneat/author', 'App\\Modules\\Author', $this->app->basePath('/app/Modules/Author')],
         ])
         ->assertSuccessful();
 });
 
-//it('outputs a table of vendor modules', function () {
-//    $this->mock(ModulesRepository::class, function(MockInterface $mock) {
-//        $mock->shouldReceive('buildVendorModulesManifest')->once();
-//    });
-//    $this->artisan('module:list')
-//        ->expectsOutputToContain('Vendor modules cached!')
-//        ->assertSuccessful();
-//});
-//
-//it('outputs a table of all modules', function () {
-//    $this->mock(ModulesRepository::class, function(MockInterface $mock) {
-//        $mock->shouldReceive('buildVendorModulesManifest')->once();
-//    });
-//    $this->artisan('module:list')
-//        ->expectsOutputToContain('Vendor modules cached!')
-//        ->assertSuccessful();
-//});
+it('outputs a table of vendor modules', function () {
+    $this->artisan('module:list --vendor')
+        ->expectsTable(['Package Name', 'Namespace', 'Path'], [
+            ['laraneat/foo', 'Laraneat\\Foo', $this->app->basePath('/vendor/laraneat/foo')],
+            ['laraneat/bar', 'Laraneat\\Bar', $this->app->basePath('/vendor/laraneat/bar')],
+        ])
+        ->assertSuccessful();
+});
+
+it('outputs a table of all modules', function () {
+    $this->artisan('module:list')
+        ->expectsTable(['Package Name', 'Namespace', 'Path'], [
+            ['laraneat/foo', 'Laraneat\\Foo', $this->app->basePath('/vendor/laraneat/foo')],
+            ['laraneat/bar', 'Laraneat\\Bar', $this->app->basePath('/vendor/laraneat/bar')],
+            ['laraneat/article', 'App\\Modules\\Article', $this->app->basePath('/app/Modules/Article')],
+            ['laraneat/author', 'App\\Modules\\Author', $this->app->basePath('/app/Modules/Author')],
+        ])
+        ->assertSuccessful();
+});
